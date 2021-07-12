3f056eaed38631bca89d72c095735b22d6c2890e
Travis Steinmetz <tsteinmetz@tsteinmetz-mbp15.guidewire.com>
2021-07-12 11:35:52 -0400

    updating lambda

--- python/hello-world-python3/lambda_function.py
+++ python/hello-world-python3/lambda_function.py
@@ -4,9 +4,9 @@ print('Loading function')
 
 
 def lambda_handler(event, context):
-    #print("Received event: " + json.dumps(event, indent=2))
-    print("value1 = " + event['key1'])
-    print("value2 = " + event['key2'])
-    print("value3 = " + event['key3'])
-    return event['key1']  # Echo back the first key value
+    print("Received event: " + json.dumps(event, indent=2))
+    print(f"value1 {event['key1']}")
+    print(f"value2 {event['key2']}")
+    print(f"value3 {event['key3']}")
+    return f"{event['key1']} {event['key2']} {event['key3']}"   # Echo back the first key value
     #raise Exception('Something went wrong')
